3cdce21203f25512d8316782d867aa5e08b06c10
Laksh Jadhwani <lakshjadhwani.lj@gmail.com>
2024-11-30 21:06:29 -0800

    Added Redux State Handling, Sidebar Collapse Functionality

--- client/src/app/dashboardWapper.tsx
+++ client/src/app/dashboardWapper.tsx
@@ -1,14 +1,41 @@
+'use client';
+
 import Navbar from '@/components/navbar/navbar';
 import Sidebar from '@/components/sidebar/sidebar';
+import StoreProvider, { useAppSelector } from './redux'
+import { useEffect } from 'react'
 
-export default function DashboardWapper({ children }: { children: React.ReactNode }) {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.add('light');
+    }
+  });
+
+
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div className={`${isDarkMode ? 'dark' : 'light'} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar />
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
+      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? 'md:pl-24' : 'md:pl-72'}`}>
         <Navbar />
         {children}
       </main>
     </div>
+  );
+}
+
+
+export default function DashboardWapper({ children }: { children: React.ReactNode }) {
+  return (
+    <StoreProvider>
+      <DashboardLayout>
+        {children}
+      </DashboardLayout>
+    </StoreProvider>
   );
 }
--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,14 +1,26 @@
 'use client';
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSideBarCollapsed } from '@/state'
 import { Bell, Menu, Settings, Sun } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 export default function Navbar() {
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+
+  const toggleSidebar = () => {
+    dispatch(setIsSideBarCollapsed(!isSidebarCollapsed));
+  }
+
+  const sidebarClassNames = `fixed flex flex-col ${isSidebarCollapsed ? 'w-0 md:w-16' : 'w-72 md:w-64'} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
   return (
     <div className='flex justify-between items-center w-full mb-7'>
       {/* LEFT SIDE */}
       <div className='flex justify-between items-center gap-5'>
-        <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => {}}>
+        <button className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar}>
           <Menu className='w-4 h-4' />
         </button>
       
