939f35e4f22c02e4caa5e21d808e927404a6cb04
Laksh Jadhwani <lakshjadhwani.lj@gmail.com>
2024-12-06 11:10:05 -0800

    Added Stats Card

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import ExpenseSummary from '@/components/expensesummary/expensesummary'
+import ExpenseSummary from '@/components/expensesummary/expensesummary';
 import PopularProducts from '@/components/popularproducts/popularproducts';
 import PurchaseSummary from '@/components/purchasesummary/purchasesummary';
-import SalesSummary from '@/components/salessummary/salessummary'
+import SalesSummary from '@/components/salessummary/salessummary';
+import StatsCard from '@/components/statscard/statscard';
+import { CheckCircle, Package, Tag, TrendingDown, TrendingUp } from 'lucide-react';
 
 export default function Dashboard() {
 	return (
@@ -12,9 +14,33 @@ export default function Dashboard() {
 			<SalesSummary />
 			<PurchaseSummary />
 			<ExpenseSummary />
-			<div className='md:row-span-1 xl:row-span-2 bg-gray-500'></div>
-			<div className='md:row-span-1 xl:row-span-2 bg-gray-500'></div>
-			<div className='md:row-span-1 xl:row-span-2 bg-gray-500'></div>
+			<StatsCard
+				title='Customer & Expenses'
+				primaryIcon={<Package className='text-blue-600 w-6 h-6' />}
+				dateRange='22 - 29 October 2023'
+				details={[
+					{ title: 'Customer Growth', amount: '175.00', changePercentage: 131, IconComponent: TrendingUp },
+					{ title: 'Expenses', amount: '10.00', changePercentage: -56, IconComponent: TrendingDown },
+				]}
+			/>
+			<StatsCard
+				title='Dues & Pending Orders'
+				primaryIcon={<CheckCircle className='text-blue-600 w-6 h-6' />}
+				dateRange='22 - 29 October 2023'
+				details={[
+					{ title: 'Dues', amount: '250.00', changePercentage: 131, IconComponent: TrendingUp },
+					{ title: 'Pending Orders', amount: '147.00', changePercentage: -56, IconComponent: TrendingDown },
+				]}
+			/>
+			<StatsCard
+				title='Sales & Discount'
+				primaryIcon={<Tag className='text-blue-600 w-6 h-6' />}
+				dateRange='22 - 29 October 2023'
+				details={[
+					{ title: 'Sales', amount: '1000.00', changePercentage: 20, IconComponent: TrendingUp },
+					{ title: 'Discount', amount: '200.00', changePercentage: -10, IconComponent: TrendingDown },
+				]}
+			/>
 		</div>
 	);
 }
